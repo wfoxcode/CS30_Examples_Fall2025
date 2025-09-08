840d09750efffc2e3e19c2bf387543f314e61de4
Webster Fox <147782599+wfoxcode@users.noreply.github.com>
2025-09-08 09:47:31 -0600

    monday demo

---
+++ MondayDemo/src/MyFirstClass.java
@@ -0,0 +1,7 @@
+public class MyFirstClass {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello there");
+        System.out.println("Welcome to Comp Sci");
+        System.out.print("This is Miller Comp");
+    }
+}
