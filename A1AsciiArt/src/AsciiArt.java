fe87b80dfb556baccbf8b43f355ba540b6ec66fc
Webster Fox <147782599+wfoxcode@users.noreply.github.com>
2025-09-09 09:36:15 -0600

    demo complete, a1 started

---
+++ A1AsciiArt/src/AsciiArt.java
@@ -0,0 +1,9 @@
+// Name:    Mr. Fox
+// Date:    9 Sept 2025   
+// Program: Assignment 1 Ascii Art
+
+public class AsciiArt {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
--- MondayDemo/src/MyFirstClass.java
+++ MondayDemo/src/MyFirstClass.java
@@ -1,7 +1,18 @@
+//  Name:       Mr. Fox
+//  Date:       9 Sept 2025
+//  Program:    Monday and Tuesday Output Demo
+
 public class MyFirstClass {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello there");
         System.out.println("Welcome to Comp Sci");
-        System.out.print("This is Miller Comp");
+        System.out.println("This is Mi\"ll\\er \'Comp");
+
+        // This is the second half of the outputs
+        System.out.println("\tthe line is indented");
+        System.out.println("This is the second part");
+        System.out.print("This is the second day.");
+
+
     }
 }
