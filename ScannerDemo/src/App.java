50cb2a819a7ea8a34d1e3b949140de3bf4da9ba4
Webster Fox <147782599+wfoxcode@users.noreply.github.com>
2025-09-19 08:52:23 -0600

    scanner demo

---
+++ ScannerDemo/src/App.java
@@ -0,0 +1,16 @@
+// Scanner and Input Demo
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner keys = new Scanner(System.in);
+
+        System.out.println("Enter an string::");
+        String inp = keys.next();
+
+        System.out.println("You entered: " + inp);
+
+
+
+    }
+}
