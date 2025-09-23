e733492813282bb1a9b853a4077d89aaec2dc181
Webster Fox <147782599+wfoxcode@users.noreply.github.com>
2025-09-23 09:21:01 -0600

    methods demo recap

---
+++ MethodsDemo2/src/Turkey.java
@@ -0,0 +1,11 @@
+public class Turkey {
+
+    public void name() {
+        System.out.println("Big Bird is the nane.");
+        speak();
+    }
+
+    public void speak() {
+        System.out.println("Quack QUack I'm a turkey");
+    }
+}
