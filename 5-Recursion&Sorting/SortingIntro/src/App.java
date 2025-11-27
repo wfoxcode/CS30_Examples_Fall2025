744889de2dfef7f7b4d608326d66bd4124a6485e
Webster Fox <147782599+wfoxcode@users.noreply.github.com>
2025-11-27 09:10:58 -0600

    sorting

--- 5-Recursion&Sorting/SortingIntro/src/App.java
+++ 5-Recursion&Sorting/SortingIntro/src/App.java
@@ -11,8 +11,18 @@ public class App {
         }
         System.out.println(Arrays.toString(nums)); //prints out array in rough form
         // ------------------------------------------------
-        
 
+        for (int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++){
+                if (nums[j-1] > nums[j]) { //SWAP
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(nums)); //prints out array in rough form
 
     } 
 }
