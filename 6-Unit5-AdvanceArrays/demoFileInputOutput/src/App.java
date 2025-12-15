e370006fd2601f8e4f1735d1da80da3450008f49
Webster Fox <147782599+wfoxcode@users.noreply.github.com>
2025-12-15 08:50:51 -0600

    friday end

--- 6-Unit5-AdvanceArrays/demoFileInputOutput/src/App.java
+++ 6-Unit5-AdvanceArrays/demoFileInputOutput/src/App.java
@@ -4,11 +4,14 @@ import java.io.*;
 public class App {
     public static void main(String[] args) throws Exception {
         String[] names = new String[5];
+        String fileName = "names.txt";
 
         names = userCreateList(names);
         printNamesList(names);
+        saveListToFile(names, fileName);
 
-    } // end of main method -------
+
+    } // end of main method ----------------------------------------
 
     public static String[] userCreateList(String[] names) {
         Scanner keys = new Scanner(System.in);
@@ -17,8 +20,9 @@ public class App {
             System.out.print("Please enter the name: ");
             names[i] = keys.nextLine();
         }
+        System.out.println();
         return names;
-    } // end of create method
+    } // end of create method ----------------------------------------
 
     public static void printNamesList(String[] names) {
         System.out.println("The names recorded are: ");
@@ -26,7 +30,22 @@ public class App {
             System.out.println("- " + names[i]);
         }
         System.out.println();
-    }
+    } //end of print method -----------------------------------
+
+    public static void saveListToFile(String[] names, String file) {
+
+        try ( PrintWriter writer = new PrintWriter( new FileWriter(file))) {
+            for (int i = 0; i < names.length; i++) {
+                writer.println(names[i]);
+            }
+            System.out.println("File save was successful.");
+        } catch (Exception e) {
+            System.out.println("An error occured while writing to file.");
+            e.printStackTrace();
+        }
+
+        System.out.println();
+    } //end of save method ---------------------------------------------------
 
 
 } // end of class -------------------------------
